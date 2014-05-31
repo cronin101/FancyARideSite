db3a29c81db80a1d28a4fffba72573d02ef04532
James Hulme <github@jkhulme.fastmail.fm>
2014-05-31 13:09:00 +0100

    made some suggested changes to the poll.  Cleared out some bogus results, renamed the results file to be json

--- routes/poll.rb
+++ routes/poll.rb
@@ -6,7 +6,7 @@ class FancyARide < Sinatra::Application
   end
 
   post '/poll' do
-    File.open('poll_results.txt', 'a') { |file| file.puts JSON.generate(params) }
+    File.open('poll_results.json', 'a') { |file| file.puts JSON.generate(params) }
     haml :poll
   end
 end
